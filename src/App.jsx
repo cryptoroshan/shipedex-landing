1b585854d14b9e64b8eaa5734266612cbbbe6ce9
tinyphoniex <royalguy1031@gmail.com>
2023-08-18 01:07:50 +0900

    modify responsive

--- src/App.jsx
+++ src/App.jsx
@@ -41,21 +41,44 @@ function App() {
                   solutions, powered by frenship
                 </p>
               </div>
-              <p className="text-xl font-bold text-white">EXCHANGE</p>
+              <p className="text-xl font-bold text-white z-50 drop-shadow-[0_4px_4px_rgba(0,0,0,0.5)] cursor-pointer hover:text-slate-300 w-fit mx-auto">
+                EXCHANGE
+              </p>
             </div>
           </section>
         </SwiperSlide>
         <SwiperSlide>
-          <section className="relative flex flex-col justify-center w-full lg:min-h-screen bg-[url('/images/page2-background.png')] bg-cover bg-no-repeat bg-center">
+          <section className="relative flex flex-col lg:justify-center w-full min-h-screen bg-[url('/images/page2-background.png')] bg-cover bg-no-repeat bg-center">
             <img
-              className="absolute left-0 right-0 top-0 bottom-0 m-auto w-1/3 lg:w-[calc((100vw-350px)/2-100px)]"
+              className="hidden lg:block absolute left-0 right-0 top-0 bottom-0 m-auto w-1/3 lg:w-[calc((100vw-350px)/2-100px)]"
               src="/images/bone.png"
             />
             <img
               className="max-md:mt-12 w-3/4 lg:w-1/3 mx-auto"
               src="/images/page2-title.png"
             />
-            <div className="flex flex-col gap-16 xl:gap-[150px] 2xl:gap-[264px] px-4 xl:px-[50px] 2xl:px-[175px] mt-4 lg:mt-[74px] text-center text-sm lg:text-xl xl:text-2xl 2xl:text-3xl font-semibold max-md:mb-12">
+            <img
+              className="block lg:hidden w-2/3 mx-auto"
+              src="/images/bone.png"
+            />
+            <ol className="block lg:hidden list-decimal text-justify w-2/3 mx-auto">
+              <li className="text-[#DF9B35]">
+                Liquidity pool voters received $BONE rewards from bribes.
+              </li>
+              <li className="text-[#3F9CF2] mt-4">
+                Increasing value of sticky bribes drives demand for veSHIP to
+                receive bribes and pool fees.
+              </li>
+              <li className="text-[#5BA000] mt-4">
+                This drives up demand for $SHIP and its price. $SHIP emissions
+                are now more valuable.
+              </li>
+              <li className="text-[#CD49B8] mt-4">
+                More $BONE in the protocol and TVL expands. Users provide
+                liquidity to gain free access to $SHIP.
+              </li>
+            </ol>
+            <div className="hidden lg:flex flex-col gap-16 xl:gap-[150px] 2xl:gap-[264px] px-4 xl:px-[50px] 2xl:px-[175px] mt-4 lg:mt-[74px] text-center text-sm lg:text-xl xl:text-2xl 2xl:text-3xl font-semibold max-md:mb-12">
               <div className="flex flex-row justify-between">
                 <p className="text-[#DF9B35] w-1/3">
                   Liquidity pool voters received $BONE rewards from bribes.
@@ -79,7 +102,7 @@ function App() {
           </section>
         </SwiperSlide>
         <SwiperSlide>
-          <section className="flex flex-col justify-center w-full lg:min-h-screen bg-[url('/images/page3-background.png')] bg-cover bg-no-repeat bg-center">
+          <section className="flex flex-col lg:justify-center w-full min-h-screen bg-[url('/images/page3-background.png')] bg-cover bg-no-repeat bg-center">
             <img
               className="w-full lg:w-2/3 mx-auto mt-[129px]"
               src="/images/roadmap.svg"
@@ -87,7 +110,7 @@ function App() {
           </section>
         </SwiperSlide>
         <SwiperSlide>
-          <section className="flex flex-col justify-center w-full py-12 lg:min-h-screen bg-[url('/images/page4-background.png')] bg-cover bg-no-repeat bg-center">
+          <section className="flex flex-col lg:justify-center w-full py-12 min-h-screen bg-[url('/images/page4-background.png')] bg-cover bg-no-repeat bg-center">
             <img className="w-[352px] mx-auto" src="/images/shiba-pepe.png" />
             <p className="text-2xl lg:text-6xl font-bold text-black opacity-80 mt-[23px] text-center">
               JOIN US IN OUR MISSION
